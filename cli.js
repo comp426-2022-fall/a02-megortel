e67590c6522ce2d71853a9eca74669056d9888ff
Megan Ortel <69562754+megortel@users.noreply.github.com>
2022-10-24 17:07:47 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -42,7 +42,7 @@ if(args.t){
 
 
 // Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&current_weather=true&timezone=' + timezone);
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&timezone=' + timezone);
 
 const data = await response.json();
 
